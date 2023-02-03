aa91c8080e1ff008d7b43713b0502a09027d7b7b
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-02-03 12:05:03 +0100

    merge

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,4 @@
 import {
-<<<<<<< HEAD
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Res,
-} from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
-import { Response } from "express";
-=======
     Controller,
     Get,
     Post,
@@ -21,54 +6,49 @@ import { Response } from "express";
     Patch,
     Param,
     Delete,
+    Res,
     ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
->>>>>>> e31e5f2f0b785fe406ed56be260d56a80403ac9c
+import { Response } from 'express';
 
-@Controller("users")
+@Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
-    const comparePass =
-      createUserDto.password === createUserDto.confirmPassword;
-    if (comparePass) {
-      return this.usersService.register(createUserDto);
+    @Post()
+    create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+        const comparePass =
+            createUserDto.password === createUserDto.confirmPassword;
+        if (comparePass) {
+            return this.usersService.register(createUserDto);
+        }
+        return res.status(401).json({
+            status: '401',
+            message:
+                'The password confirmation is not the same as the password !!',
+        });
     }
-    return res.status(401).json({
-      status: "401",
-      message: "The password confirmation is not the same as the password !!",
-    });
-  }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.usersService.findAll();
+    }
 
-<<<<<<< HEAD
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.usersService.findOne(+id);
-  }
-=======
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id: number) {
         return this.usersService.findOne(+id);
     }
->>>>>>> e31e5f2f0b785fe406ed56be260d56a80403ac9c
 
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.usersService.update(+id, updateUserDto);
+    }
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.usersService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.usersService.remove(+id);
+    }
 }
